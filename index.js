aacfc18c215613285232c11342ae973ec1605e4b
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-10-29 19:22:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, event)
+{
+  let thankYouArray = [];
+  for (let i = 0; i < names.length; i++)
+  {
+    thankYouArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  
+  return thankYouArray;
+}
+
+function countDown(startingPoint)
+{
+  while (startingPoint >= 0)
+  {
+    console.log(startingPoint);
+    startingPoint--;
+  }
+}
