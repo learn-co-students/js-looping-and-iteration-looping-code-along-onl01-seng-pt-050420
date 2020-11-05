87b789ea8e6112046a74c571c2d0c62f455628ca
Catheryn Small <catheryn.small@gmail.com>
2020-11-04 21:02:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, e_name) {
+    let msg = []
+    for (let c = 0; c < names.length; c++) {
+        msg.push(`Thank you, ${names[c]}, for the wonderful ${e_name} gift!`)
+    }
+    return msg;
+}
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
