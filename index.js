9ca1675d9d9a0692d360241fa5e78f7d83169e86
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-11-08 18:37:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names) {
+  let x = []
+  for (let i = 0; i <= names.length - 1; i++){
+   x[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+  }
+  return x
+}
+
+function countDown(number) {
+  let i = 0
+  while(i <= number) {
+    console.log(number - i)
+    i ++
+  }
+  
+}
+
